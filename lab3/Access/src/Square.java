ad8725015a89c93b69fdad2015d0fcdbc938a2b2
Mehmet Ali Özer <mehmetali.ozer@std.yeditepe.edu.tr>
2020-05-31 22:57:53 +0300

    update accessor and mutators

--- lab3/Access/src/Square.java
+++ lab3/Access/src/Square.java
@@ -5,10 +5,12 @@ public class Square {
 		distance = len;
 	}
 	
+	//accessor method
 	public double getDistance() {
 		return distance;
 	}
 	
+	//modifier method
 	public void setDistance(double new_distance) {
 		distance = new_distance;
 	}
